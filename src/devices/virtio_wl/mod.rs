ffa6075c8090a34f751866c18e7ca8cb5308390a
Bruce Leidl <bruce@subgraph.com>
2023-01-27 16:34:34 -0500

    VIRTIO_ID_WL changed to match ID value in kernel driver.

--- src/devices/virtio_wl/mod.rs
+++ src/devices/virtio_wl/mod.rs
@@ -14,7 +14,7 @@ mod device;
 mod consts {
     use std::mem;
 
-    pub const VIRTIO_ID_WL: u16 = 30;
+    pub const VIRTIO_ID_WL: u16 = 63;
     pub const VIRTWL_SEND_MAX_ALLOCS: usize = 28;
     pub const VIRTIO_WL_CMD_VFD_NEW: u32 = 256;
     pub const VIRTIO_WL_CMD_VFD_CLOSE: u32 = 257;
